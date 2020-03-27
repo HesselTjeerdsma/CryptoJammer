e6ba95b83bbc3aefcc4eada11a9d5eb41b4c4983
Hessel Tjeerdsma <hesseltjeerdsma@gmail.com>
2020-03-27 15:28:21 +0100

    .gitignore is now working

--- ransomware/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import InfectedPc
-
-admin.site.register(InfectedPc)
-# Register your models here.
--- ransomware/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RansomwareConfig(AppConfig):
-    name = 'ransomware'
