0f5fb86fc3d7c7af6cde56f2924f58d8b295ea1e
Hessel Tjeerdsma <hesseltjeerdsma@gmail.com>
2020-03-27 15:26:10 +0100

    .gitignore is now working

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\hesse\Documents\Code\ransomware_service\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
